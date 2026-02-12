e3a43bec70fba90f509c4215a3adddae22080c69
LeonamGiocondo <leonamg59@outlok.com>
2026-02-12 13:58:31 -0300

    Semana1 Dia1 — Variáveis

---
+++ Semana1/Dia1_Variaveis.py
@@ -0,0 +1,13 @@
+# 1st - Variables
+# Objectiv - Create variables and print their values
+
+# Create variables for name, age and city
+
+
+name = "Leonam"
+age = "28"
+city = "Brussels"
+
+# Print a sentence using these variables
+
+print("Hello, my name's", name, ", I'm", age, "years old and I live in", city)

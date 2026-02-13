9f95bba2236eb09eda8e29b4c0f6989baf0b0a5a
LeonamGiocondo <leonamg59@outlok.com>
2026-02-13 18:55:06 -0300

    Week1 Day2 — Types of Data

---
+++ Week1/Day2_Types.py
@@ -0,0 +1,27 @@
+# Day 2 - Types of Data
+# Objective: Create variables of differents types and print them
+
+# 
+
+# Interger 
+
+age = 28
+
+# Float
+
+height = 1.85
+
+# String
+
+name = "Leonam"
+
+# Boolean
+
+is_adult = True
+
+# Print all values
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Adult:", is_adult)

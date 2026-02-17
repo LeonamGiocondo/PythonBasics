3286f8c63c8f83e53b267ceab03024256aaf8723
LeonamGiocondo <leonamg59@outlok.com>
2026-02-17 08:57:57 -0300

    Day 5 - Password Checker

--- Week1/Day5_PasswordChecker.py
+++ Week1/Day5_PasswordChecker.py
@@ -3,14 +3,22 @@
 password = input("Enter your password: ")
 
 has_number = False
+has_uppercase = False
 
 for char in password:
 
     if char.isdigit():
        has_number = True
 
+    if char.isupper():
+       has_uppercase = True
 
-if len(password) >= 8 and has_number:
+
+if len(password) >= 8 and has_number and has_uppercase:
     print("Password strength: Strong")
+
+elif len(password) >= 8 and has_number:
+    print("Password strength: Medium")
+
 else:
     print("Password strenght: Weak")

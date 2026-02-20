597db8e3d917656ae03e04cbd4d78835fb7c63f4
LeonamGiocondo <leonamg59@outlok.com>
2026-02-20 12:07:58 -0300

    Week1 Day7 - Guessing Game

---
+++ Week1/Day7_GuessingGame.py
@@ -0,0 +1,18 @@
+# Day 7 - Number Guessing Game
+
+secret_number = 7
+
+guess = 0
+
+while guess != secret_number:
+
+    guess = int(input("Guess the number: "))
+
+    if guess > secret_number:
+        print("Too high")
+
+    elif guess < secret_number:
+        print("Too low")
+
+    else:
+        print("Correct! You guessed it.")

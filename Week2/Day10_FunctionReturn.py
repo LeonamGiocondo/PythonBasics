2fbb75cce098ea8f382e1614532f0f0cd68608d2
LeonamGiocondo <leonamg59@outlok.com>
2026-02-23 16:28:15 -0300

    Day 10 - Function Return

--- Week2/Day10_FunctionReturn.py
+++ Week2/Day10_FunctionReturn.py
@@ -2,10 +2,16 @@
 
 def add_numbers(a, b):
     return a + b
+def square(number):
+    return number * number
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
+num = int(input("Enter a number: "))
+result = square(num)
+
 result = add_numbers(num1, num2)
 
 print("Result:", result)
+print("Square:", result)

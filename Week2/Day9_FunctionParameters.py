e090404d27ccc7d717156726853f8f7318b73820
LeonamGiocondo <leonamg59@outlok.com>
2026-02-23 16:02:56 -0300

    Day 9 - Functions Parameters Flex

--- Week2/Day9_FunctionParameters.py
+++ Week2/Day9_FunctionParameters.py
@@ -4,7 +4,12 @@ def greet(name):
     print("Hello,", name)
     print("Welcome!")
 
+def show_age(age):
+    print("You are", age, "years old")
+
 
 name = input("Enter your name: ")
+age = input("How old are you? ")
 
 greet(name)
+show_age(age)

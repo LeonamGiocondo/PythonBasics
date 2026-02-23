03bca704535081bb05e37f14b8de1aab42c1feb5
LeonamGiocondo <leonamg59@outlok.com>
2026-02-23 16:21:49 -0300

    Day 10 - Function Return

---
+++ Week2/Day10_FunctionReturn.py
@@ -0,0 +1,11 @@
+# Day 10 — Function Return
+
+def add_numbers(a, b):
+    return a + b
+
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = add_numbers(num1, num2)
+
+print("Result:", result)

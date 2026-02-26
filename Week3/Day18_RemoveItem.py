d1e6e0a32a3850777abd61d1f972e7971d54f593
LeonamGiocondo <leonamg59@outlok.com>
2026-02-26 11:35:38 -0300

    Day 18 - Exercices

---
+++ Week3/Day18_RemoveItem.py
@@ -0,0 +1,33 @@
+# Remove 
+
+fruits = ["Banana", "Apple", "Grape", "Orange"]
+
+fruits.remove("orange")  # remove 
+
+fruits.pop(0)  # remove
+
+
+print(fruits)
+
+
+-----
+
+# Show how much fruits
+
+fruits = ["Banana", "Apple", "Grape", "Orange", "Mango"]
+
+print(len(fruits))
+
+
+
+-----
+
+# Show fruits, quantity, initial and final number
+
+fruits = ["Banana", "Apple", "Grape", "Orange", "Mango"]
+print(fruits)
+print(len(fruits))
+print(fruits[0])
+print(fruits[4])
+
+

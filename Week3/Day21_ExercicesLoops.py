43938a212377f9fb7d3e722fa43a6b134f42ed7a
LeonamGiocondo <leonamg59@outlok.com>
2026-03-04 09:09:23 -0300

    Day 21 - Exercices Loops w/ Lists

---
+++ Week3/Day21_ExercicesLoops.py
@@ -0,0 +1,28 @@
+# Day 19 - Exercices Loops w/ List w/ for and enumerate
+
+# Print each fruit on a separate line
+
+fruits = ["Banana","Apple","Grape","Orange","Mango"]
+
+for fruit in fruits:
+    print(fruit)
+
+# ----
+
+# print index: fruit using enumerate
+
+fruits = ["Banana", "Apple", "Grape", "Orange", "Mango"]
+
+for index, fruit in enumerate(fruits):
+    print(f"{index}: {fruit}")
+
+
+# ----
+
+
+# Using enumerate(start=1), print 1) Banana, 2) Apple ... 
+
+fruits = ["Banana", "Aple", "Grape", "Orange", "Mango"]
+
+for index, fruit in enumerate(fruits, start=1):
+    print(f"{index}: {fruit}")
